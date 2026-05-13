77cbecc4a1d37d6c0e846088b9d84f9a17ac2268
Winnie173 <Winnie173@users.noreply.github.com>
2026-05-13 23:05:06 +0800

    💊 吃药提醒 App - 初始版本

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='med-v1';
+const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(clients.claim());});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)).catch(()=>caches.match('./index.html')));
+});
